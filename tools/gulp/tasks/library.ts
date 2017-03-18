5934d808b008be845a49b0cce5aa2fb8f8ae07bc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-03-18 22:17:21 +0100

    Build assets and inline them

--- tools/gulp/tasks/library.ts
+++ tools/gulp/tasks/library.ts
@@ -2,7 +2,7 @@ import {task, src, dest} from 'gulp';
 import {join} from 'path';
 import {main as ngc} from '@angular/compiler-cli';
 import {SOURCE_ROOT, DIST_ROOT, UGLIFYJS_OPTIONS} from '../constants';
-import {sequenceTask} from '../util/task_helpers';
+import {sequenceTask, sassBuildTask, copyTask} from '../util/task_helpers';
 import {createRollupBundle} from '../util/rollup-helper';
 import {transpileFile} from '../util/ts-compiler';
 import {ScriptTarget, ModuleKind} from 'typescript';
@@ -10,6 +10,7 @@ import {ScriptTarget, ModuleKind} from 'typescript';
 // There are no type definitions available for these imports.
 const gulpUglify = require('gulp-uglify');
 const gulpRename = require('gulp-rename');
+const inlineResources = require('../../../scripts/release/inline-resources');
 
 const libraryRoot = join(SOURCE_ROOT, 'lib');
 const tsconfigPath = join(libraryRoot, 'tsconfig.json');
@@ -26,6 +27,21 @@ const umdBundleFile = join(bundlesDir, 'material.umd.js');
 
 task('library', sequenceTask(
   'clean',
+  ['library:build:esm', 'library:assets'],
+  // Inline assets into ESM output.
+  'library:assets:inline',
+  // Build bundles on top of inlined ESM output.
+  'library:build:fesm-2015',
+  'library:build:fesm-2014',
+  'library:build:umd',
+  'library:build:umd:min'
+));
+
+/**
+ * TypeScript Compilation Tasks. Tasks are creating ESM, FESM, UMD bundles for releases.
+ **/
+
+task('library:build', sequenceTask(
   'library:build:esm',
   'library:build:fesm-2015',
   'library:build:fesm-2014',
@@ -61,3 +77,13 @@ task('library:build:umd:min', () => {
     .pipe(gulpRename({suffix: '.min'}))
     .pipe(dest(bundlesDir));
 });
+
+/**
+ * Asset Tasks. Building SaSS files and inlining CSS, HTML files into the ESM output.
+ **/
+
+task('library:assets', ['library:assets:scss', 'library:assets:html']);
+
+task('library:assets:scss', sassBuildTask(materialDir, libraryRoot, true));
+task('library:assets:html', copyTask(join(libraryRoot, '**/*.+(html|scss)'), materialDir));
+task('library:assets:inline', () => inlineResources(materialDir));
